4d9c1a00b94445a3ca000d0c8efb1f323dbe4109
Toby Liu <ybot1122@gmail.com>
2024-01-28 20:12:50 -0800

    Attempt Stripe hit

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,7 +1,20 @@
-export default async (req, context) => {
-  const processEnv = process.env.STRIPE_SECRET
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
-  const NetlifyEnv = Netlify.env.get("STRIPE_SECRET");
-  
-  return new Response("processEnv: " + processEnv + "\n netlifyEnv: " + NetlifyEnv);
+const YOUR_DOMAIN = 'https://moleculeworks.com/';
+
+export default async (req, context) => {
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: 'price_1Odh8QKVWxESeTWrzztr10Yz',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  return new Response("url: " + session.url);
 };
